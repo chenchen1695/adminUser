a62ecdaa5eacabf66d9d6206fbdd380bf58c8ec7
chen <chen@bjpowernode.com>
2019-11-09 19:53:12 +0800

    java模块初始化1提交

--- src/main/java/com/bjpowernode/model/User.java
+++ src/main/java/com/bjpowernode/model/User.java
@@ -7,4 +7,12 @@ public class User {
     private String id;
     private String name;
     private String address;
+    private String phone;
+
+    public User(String id, String name, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
 }
